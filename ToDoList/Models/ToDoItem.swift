c46512c7090c0afcbd995a0e7e67bbc1a7802d67
sprh <45209070+sprh@users.noreply.github.com>
2021-06-25 14:26:39 +0300

    Some corrections after merge: ToDoItem: I add a field done and change a return value (add this field and change init for a deadline) in parse

--- ToDoList/Models/ToDoItem.swift
+++ ToDoList/Models/ToDoItem.swift
@@ -18,6 +18,7 @@ struct ToDoItem {
     var deadlineSince1970: Int? {
         return Int(deadline?.timeIntervalSince1970 ?? 0)
     }
+    var done: Bool
     var color: String
     /// - Parameters:
     ///     - id: An unique user id. Default value is UUID().uuidString
@@ -76,7 +77,11 @@ extension ToDoItem {
         let deadline = data.keys.contains("deadline") ? data["deadline"] as? Double : nil
         let color = data.keys.contains("color") ? data["color"] as? String : "#%06x"
         let done = data.keys.contains("done") ? data["done"] as? Bool : false
-        return ToDoItem(id: id, text: text, importance: importance, deadline: deadline ?? nil,
-                        color: color ?? "#%06x", done: done ?? false)
+        return ToDoItem(id: id,
+                        text: text,
+                        importance: importance,
+                        deadline: Date.init(timeIntervalSince1970: deadline ?? 0),
+                        color: color ?? "#%06x",
+                        done: done ?? false)
     }
 }
