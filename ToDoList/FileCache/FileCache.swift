29a102330464a1fca7d7dc94366086bd821e1fa9
sprh <45209070+sprh@users.noreply.github.com>
2021-07-01 12:09:10 +0300

    Corrections after feedback

---
+++ ToDoList/FileCache/FileCache.swift
@@ -0,0 +1,102 @@
+//
+//  FileCache.swift
+//  ToDoList
+//
+//  Created by Софья Тимохина on 09.06.2021.
+//
+
+import Foundation
+
+final class FileCache {
+    /// An array of to do items.
+    private(set) var toDoItems: [ToDoItem] =
+        [ToDoItem(id: "1", text: "Купить сыр", deadline: Date(), color: "", done: false),
+         ToDoItem(id: "2", text: "Купить сыр", deadline: Date(), color: "", done: true),
+         ToDoItem(id: "3", text: "Купить сыр", color: "", done: false),
+         ToDoItem(id: "4", text: "Купить сыр", deadline: Date(), color: "", done: true),
+         ToDoItem(id: "5", text: "Купить сыр", importance: .important,
+                  deadline: nil, color: "", done: false),
+         ToDoItem(id: "6", text: "Купить что-то, где-то, зачем-то, но зачем не очень понятно, но точно чтобы показать, что необходимо купить сыр", importance:
+                    .important, deadline: nil, color: "", done: true),
+         ToDoItem(id: "7", text: "Купить сыр", color: "", done: false),
+         ToDoItem(id: "1", text: "Купить сыр", deadline: Date(), color: "", done: false),
+         ToDoItem(id: "2", text: "Купить сыр", deadline: Date(), color: "", done: true),
+         ToDoItem(id: "3", text: "Купить сыр", color: "", done: false),
+         ToDoItem(id: "1", text: "Купить сыр", deadline: Date(), color: "", done: false),
+         ToDoItem(id: "2", text: "Купить сыр", deadline: Date(), color: "", done: true),
+         ToDoItem(id: "3", text: "Купить сыр", color: "", done: false),
+         ToDoItem(id: "1", text: "Купить сыр", deadline: Date(), color: "", done: false),
+         ToDoItem(id: "2", text: "Купить сыр", deadline: Date(), color: "", done: true),
+         ToDoItem(id: "3", text: "Купить сыр", color: "", done: false)]
+    /// Add an item to the array.
+    ///  - Parameters:
+    ///  - item: A new to do item.
+    ///
+    func add(item toDoItem: ToDoItem) {
+        guard let index = toDoItems.firstIndex(where: {$0.id == toDoItem.id}) else {
+            toDoItems.append(toDoItem)
+            return
+        }
+        toDoItems[index] = toDoItem
+    }
+    /// Delete an element from the array by its id.
+    /// - Parameters:
+    /// - id: an identifire of the item.
+    func delete(with id: String) {
+        guard let index = toDoItems.firstIndex(where: {$0.id == id}) else { return }
+        toDoItems.remove(at: index)
+    }
+    func get(with id: String) -> ToDoItem? {
+        return toDoItems.first(where: {$0.id == id})
+    }
+    /// Save an array of objects to the file.
+    /// - Parameters:
+    /// - Path: a string contains the path to the file in which we save an array.
+    func saveFile(to path: String = "todoitems.json", closure: @escaping (FileCacheError) -> Void) {
+        guard let documentDirectory = FileManager.default.urls(for: .documentDirectory,
+                                                               in: .userDomainMask).first else {
+            closure(FileCacheError.fileNotFound)
+            return
+        }
+        let jsonArray = toDoItems.map { $0.json }
+        let url = documentDirectory.appendingPathComponent(path)
+        do {
+            let json = try JSONSerialization.data(withJSONObject: jsonArray, options: .prettyPrinted)
+            try json.write(to: url, options: [])
+        } catch {
+            closure(FileCacheError.canNotWrite)
+        }
+    }
+    /// Load an array of objects from the file.
+    /// - Parameters:
+    /// - Path: a string contains the path to the file from which we load an array.
+    func loadFile(from path: String = "todoitems.json", closure: @escaping (FileCacheError) -> Void) {
+        // Check if a file with the path exists.
+        guard let documentDirectory = FileManager.default.urls(for: .documentDirectory,
+                                                               in: .userDomainMask).first else {
+            closure(FileCacheError.fileNotFound)
+            return
+        }
+        // Because we don't want to store repeatable elements.
+        toDoItems.removeAll()
+        let url = documentDirectory.appendingPathComponent(path)
+        do {
+            let json = try String(contentsOf: url, encoding: .utf8)
+            let jsonData = json.data(using: String.Encoding.utf8, allowLossyConversion: false)!
+            guard let data = try
+                    JSONSerialization.jsonObject(with: jsonData, options: []) as? [Any] else {
+                print("Can't parse a json string")
+                closure(FileCacheError.canNotRead)
+                return
+            }
+            for item in data {
+                guard let toDoItem = ToDoItem.parse(json: item) else {
+                    continue
+                }
+                toDoItems.append(toDoItem)
+            }
+        } catch {
+            closure(FileCacheError.canNotRead)
+        }
+    }
+}
